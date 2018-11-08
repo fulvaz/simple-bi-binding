c5b80bcb251eff9567fcec65b5d477203013077a
fulvaz <fulvaz@foxmail.com>
2018-11-09 02:43:32 +0800

    init2

--- .history/test/index_20181109023158.ts
+++
@@ -1,6 +0,0 @@
-function BiBinding<T extends {new(...args:any[]):{}}>(constructor:T) {
-    return class extends constructor {
-        newProperty = "new property";
-        hello = "override";
-    }
-}
